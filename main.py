ebcb654a5ca020a0c9699a61031be181194c2bf3
PoisonGold91 <alvaro.dorado91@gmail.com>
2024-10-07 21:01:01 -0500

    Commit desde la rama develop / Commit from develop branch

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ x=input("Variable de entrada 1: ")
 
 y=input("Variable de entrada 2: ")
 
+z=input("Variable de entrada 3: ")
+
 x=int(x)
 y=int(y)
+z=int(z)
 
-print(f"Este es tu resultado: {x+y}")
+print(f"Este es tu resultado: {x+y+z}")
